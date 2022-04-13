82a1b131f30f14f2816f88e0d967a88f02c4fb2e
Yusuf Ekrem Keçilioğlu <yekecilioglu@gmail.com>
2022-04-13 21:20:15 +0300

    more visual clarification for opponent rays

--- Assets/Game/Scripts/Opponents/OpponentAI.cs
+++ Assets/Game/Scripts/Opponents/OpponentAI.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class OpponentAI : MonoBehaviour
 {
     [SerializeField] private int numberOfRays = 17;
-    [SerializeField] private float angle = 60;
+    [SerializeField] private float angle = 60, rayLength = 1f;
     [SerializeField] private LayerMask obstacleMask;
     private Vector3 oldPos;
+    private Vector3 moveDirection = Vector3.forward;
     // Start is called before the first frame update
     void Start()
     {
         oldPos = transform.position;
+        StartCoroutine(RandomDirectionRoutine());
     }
 
     // Update is called once per frame
@@ -19,6 +21,7 @@ public class OpponentAI : MonoBehaviour
     {
         MoveCharacter();
     }
+    //Need to add randomness to it's movement
     private void MoveCharacter()
     {
         var deltaPosition = Vector3.zero;
@@ -33,28 +36,40 @@ public class OpponentAI : MonoBehaviour
             var direction = rotation * rotationMod * Vector3.forward;
 
             var ray = new Ray(this.transform.position+ transform.up, direction);
-            if(Physics.Raycast(ray, out RaycastHit raycastHit, 1f, obstacleMask))
+            var value = i%2 == 0 ? 2f : 1f;
+            
+            if (Physics.Raycast(ray, out RaycastHit raycastHit, rayLength*value, obstacleMask))
             {
                 hit = true;
-                if(raycastHit.normal.x < 0)
+                Debug.DrawRay(transform.position + transform.up, direction*rayLength*value, Color.red, 0f);
+                if (raycastHit.normal.x < 0)
                 {
-
+                    //Lean left
                 }
                 else
                 {
-
+                    //Lean right
                 }
                 deltaPosition -= (1f / numberOfRays) * 5f * direction;
             }
             else
             {
+                Debug.DrawRay(transform.position + transform.up, direction* rayLength*value, Color.green, 0f);
                 deltaPosition += (1f / numberOfRays) * 5f * direction;
             }
 
         }
         //TODO
         Debug.Log($"{deltaPosition}, {gameObject.name}");
-        if (deltaPosition.z >= 0.5) this.transform.position += deltaPosition * Time.deltaTime;
+        if (deltaPosition.z >= 0.2)
+        {
+
+            this.transform.position += deltaPosition*Time.deltaTime;
+           //var pos = this.transform.position;
+           //pos.x += deltaPosition.x;
+           //pos.x = Mathf.Clamp(pos.x, -8, 8);
+           //this.transform.position = Vector3.Lerp(transform.position, pos, Time.deltaTime * 20f); ;
+        }
         else HandleObstacleHit();
 
     }
@@ -64,20 +79,39 @@ public class OpponentAI : MonoBehaviour
         transform.position = oldPos;
     }
 
-    private void OnDrawGizmos()
+    //TODO
+    private IEnumerator RandomDirectionRoutine()
     {
-        for (int i = 0; i <= numberOfRays; i++)
+        while (true)
         {
-            var rotation = this.transform.rotation;
-            //When i is 0 new angle will be -angle 
-            //When i is numberOfRays-1 new angle will be angle
-            var rotationMod = Quaternion.AngleAxis((i / (float)numberOfRays) * angle*2 - angle, this.transform.up);
-            var direction = rotation * rotationMod * Vector3.forward;
-
-            //Debug.Log($"Angle: {(i / (float)numberOfRays) * 60 * 2 - 60} Direction: {direction}");
-
-            Gizmos.DrawRay(this.transform.position + transform.up, direction);
+            transform.position += new Vector3(Random.Range(-1f, 1f), transform.position.y, Random.Range(0f, 1f)).normalized * Time.deltaTime*5f;
+            yield return new WaitForSeconds(3f);
 
         }
     }
+
+    //private void OnDrawGizmos()
+    //{
+    //    
+    //    for (int i = 0; i <= numberOfRays; i++)
+    //    {
+    //        var rotation = this.transform.rotation;
+    //        //When i is 0 new angle will be -angle 
+    //        //When i is numberOfRays-1 new angle will be angle
+    //        var rotationMod = Quaternion.AngleAxis((i / (float)numberOfRays) * angle*2 - angle, this.transform.up);
+    //        var direction = rotation * rotationMod * Vector3.forward;
+    //
+    //        //Debug.Log($"Angle: {(i / (float)numberOfRays) * 60 * 2 - 60} Direction: {direction}");
+    //       //if ((bool)hitArray[i])
+    //       //{
+    //       //    Gizmos.color = Color.red;
+    //       //}
+    //       //else
+    //       //{
+    //       //    Gizmos.color = Color.white;
+    //       //}
+    //        Gizmos.DrawRay(this.transform.position + transform.up, direction);
+    //
+    //    }
+    //}
 }
